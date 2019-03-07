335f8339c19f619a63d6279378a2e5252bf7e2db
vagrant <vagrant@homestead>
2019-03-07 14:42:31 +0000

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Request;
+
 class UsersController extends Controller
 {
     public function create()
@@ -11,7 +12,17 @@ class UsersController extends Controller
         return view('users/create');
     }
 
-    public function show(User $user){
-        return view('users.show',compact('user'));
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'     => 'required|max:50',
+            'email'    => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6']);
+        return;
     }
 }
