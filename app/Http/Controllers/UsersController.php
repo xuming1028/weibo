6f3ecdd96f4d06fcee14e48eae061e6e74391a7c
vagrant <vagrant@homestead>
2019-03-21 09:47:52 +0000

    管理员删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,18 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function __construct(){
-        $this->middleware('auth',[
-            'except' => ['show','create', 'store','index']
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store', 'index'],
         ]);
 
-        $this->middleware('guest',[
-            'only' => ['create']
+        $this->middleware('guest', [
+            'only' => ['create'],
         ]);
     }
     //注册界面
@@ -38,9 +39,9 @@ class UsersController extends Controller
             'email'    => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6']);
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password)
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => bcrypt($request->password),
         ]);
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
@@ -48,32 +49,43 @@ class UsersController extends Controller
     }
 
     //编辑界面
-    public function edit(User $user){
+    public function edit(User $user)
+    {
         $this->authorize('update', $user);
-        return view('users.edit',compact('user'));
+        return view('users.edit', compact('user'));
     }
 
     //修改个人信息
-    public function update(User $user,Request $request){
+    public function update(User $user, Request $request)
+    {
         $this->authorize('update', $user);
-        $this->validate($request,[
-            'name' => 'required|max:50',
-            'password' => 'nullable|confirmed|min:6'
+        $this->validate($request, [
+            'name'     => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6',
         ]);
-        $data = [];
+        $data         = [];
         $data['name'] = $request->name;
-        if($request->password){
+        if ($request->password) {
             $data['password'] = bcrypt($request->password);
         }
         $user->update($data);
-        session()->flash('success','个人资料修改成功');
-        return redirect()->route('users.show',$user);
+        session()->flash('success', '个人资料修改成功');
+        return redirect()->route('users.show', $user);
     }
 
     //用户列表
-    public function index(){
+    public function index()
+    {
         $users = User::paginate(5);
         // $users = User::all();
         return view('users.index', compact('users'));
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user->name = 'xuming';
         $user->email = '461770336@qq.com';
         $user->password = bcrypt('123456');
+        $user->is_admin = true;
         $user->save();
 
     }
