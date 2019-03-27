82dab8aa7ef74544b6f816fade07ec18f6c5e717
vagrant <vagrant@homestead>
2019-03-27 09:27:06 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,54 @@ class User extends Authenticatable
     public function feed(){
         return $this->statuses()->orderBy('created_at','desc');
     }
+
+    //获取粉丝列表（关注自己的用户）
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    /**
+     * [followings 获取用户列表（关注这个粉丝的用户）]
+     * @return [type] [description]
+     */
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+    /**
+     * [follow 关注]
+     * @param  [type] $user_ids [description]
+     * @return [type]           [description]
+     */
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    /**
+     * [unfollow 取消关注]
+     * @param  [type] $user_ids [description]
+     * @return [type]           [description]
+     */
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    /**
+     * [isFollowing 判断是否存在关注]
+     * @param  [type]  $user_id [description]
+     * @return boolean          [description]
+     */
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
